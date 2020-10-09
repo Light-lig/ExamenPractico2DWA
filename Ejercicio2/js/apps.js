6be96d12b2ddcd62b14ae63e8ed7cd275468903b
Toberal98 <douglazalberto78@gmail.com>
2020-10-08 22:10:16 -0600

    formulario contactenos funcional

--- Ejercicio2/js/apps.js
+++ Ejercicio2/js/apps.js
@@ -1,6 +1,4 @@
-window.onload = () => {
-swal("Bienvenido!!!!", "Espero disfrute el contenido.");
-}
+
 $("#form-registro").on("submit",function(event){
     event.preventDefault();
     swal("Good job!", "Has creado tu cuenta con exito", "success");
